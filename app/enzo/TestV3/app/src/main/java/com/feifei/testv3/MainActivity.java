40db7308697514b6d1234ea504dcfb50073ff94a
alcantaraenzoeee <lorenzo.alcantara@eee.upd.edu.ph>
2021-04-05 02:44:26 +0800

    Added Discoverability Button for Kris

--- app/enzo/TestV3/app/src/main/java/com/feifei/testv3/MainActivity.java
+++ app/enzo/TestV3/app/src/main/java/com/feifei/testv3/MainActivity.java
@@ -2,11 +2,17 @@ package com.feifei.testv3;
 
 import android.Manifest;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.le.AdvertiseCallback;
+import android.bluetooth.le.AdvertiseData;
+import android.bluetooth.le.AdvertiseSettings;
+import android.bluetooth.le.BluetoothLeAdvertiser;
 import android.bluetooth.le.BluetoothLeScanner;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.ParcelUuid;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -19,6 +25,7 @@ import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
 
     BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
     BluetoothLeScanner bluetoothLeScanner = bluetoothAdapter.getBluetoothLeScanner();
+    BluetoothLeAdvertiser bleadvertiser = BluetoothAdapter.getDefaultAdapter().getBluetoothLeAdvertiser();
 
     private static final String TAG = "MainActivity";
     private Scanner ble_scanner;
@@ -134,5 +142,58 @@ public class MainActivity extends AppCompatActivity {
 
     private void stopScan() {
         ble_scanner.stopScan();
+    }
+
+
+    public void BT_Discoverable(View view) {
+
+        //permission check if device can advertise ble packets
+        if( !BluetoothAdapter.getDefaultAdapter().isMultipleAdvertisementSupported() ) {
+            Toast.makeText( this, "Multiple advertisement not supported", Toast.LENGTH_SHORT ).show();
+            Log.e("BLE", "Multiple ads not supported");
+        }
+        if(BluetoothAdapter.getDefaultAdapter().isMultipleAdvertisementSupported() ) {
+            Toast.makeText(this, "Multiple advertisement supported", Toast.LENGTH_SHORT).show();
+            Log.e("BLE", "Multiple ads supported");
+
+
+            AdvertiseSettings settings = new AdvertiseSettings.Builder()
+                    .setAdvertiseMode(AdvertiseSettings.ADVERTISE_MODE_LOW_LATENCY)
+                    .setTxPowerLevel(AdvertiseSettings.ADVERTISE_TX_POWER_HIGH)
+                    .setTimeout(5000)
+                    .build();
+
+            ParcelUuid self_uuid = new ParcelUuid(UUID.fromString(getString(R.string.ble_user_uuid)));
+            //UUID set for advertising is 2e952a2b-eef3-4a80-a309-6a3f5aacb1e8
+            AdvertiseData data = new AdvertiseData.Builder()
+                    .setIncludeDeviceName(false)
+                    .addServiceUuid(self_uuid)
+                    .build();
+
+            AdvertiseData advScanResponse = new AdvertiseData.Builder()
+                    .setIncludeDeviceName(true)
+                    .build();
+
+            String user_uuid = getString(R.string.ble_user_uuid);
+
+
+            AdvertiseCallback adCallback = new AdvertiseCallback() {
+                @Override
+                public void onStartSuccess(AdvertiseSettings settingsInEffect) {
+                    super.onStartSuccess(settingsInEffect);
+                    Log.e(" BLE", "On start discovery success");
+                    Toast.makeText(getApplicationContext(), "UUID is " + user_uuid + "\n Advertising set for 5 secs", Toast.LENGTH_SHORT).show();
+                }
+
+                @Override
+                public void onStartFailure(int errorCode) {
+                    Log.e("BLE", "Advertising onStartFailure: " + errorCode);
+                    super.onStartFailure(errorCode);
+                }
+            };
+
+
+            bleadvertiser.startAdvertising(settings, data, advScanResponse, adCallback);
+        }
     }
 }
