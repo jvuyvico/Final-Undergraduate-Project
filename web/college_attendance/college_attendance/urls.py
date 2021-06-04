71e0956777ea443c20d88ca7dce3a30d99c9210f
Sean Kristian Garibay <sggaribay@up.edu.ph>
2021-06-04 14:31:32 +0800

    added fromESP app

--- web/college_attendance/college_attendance/urls.py
+++ web/college_attendance/college_attendance/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from fromESP.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,4 +24,5 @@ urlpatterns = [
     path('info/', include('info.urls')),
     path('accounts/login/', auth_views.LoginView.as_view(template_name='info/login.html'), name='login'),
     path('accounts/logout/', auth_views.LogoutView.as_view(template_name='info/logout.html'), name='logout'),
+    path('esp/', fromESPview, name='esp'),
 ]
